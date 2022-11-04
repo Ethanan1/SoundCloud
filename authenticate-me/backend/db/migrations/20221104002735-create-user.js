f2eeeacaa124a6550d52dae03564d9671fed6719
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2022-11-03 20:34:59 -0700

    seeds

---
+++ authenticate-me/backend/db/migrations/20221104002735-create-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      hashedPassword: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
